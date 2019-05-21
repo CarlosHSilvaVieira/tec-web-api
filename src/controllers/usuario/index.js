ada3a25092aaad243b475ccbbac255ef689cfc91
Carlos <carlos.h.silva.vieira@gmail.com>
2019-05-21 10:25:49 -0300

    Change responses of usuarios api

--- src/controllers/usuario/index.js
+++ src/controllers/usuario/index.js
@@ -19,14 +19,14 @@ class UsuarioController {
 
     create(req, res, next) {
 
-        var venda = req.body.venda
+        var usuario = req.body.usuario
 
-        if (!venda) {
+        if (!usuario) {
             return res.status(500).json({ code: 500, id: null, error: `${this.tabela} não fornecido` })
         }
 
         var query = `insert into ${this.tabela} set ?`
-        this.mysql.query(query, { ...venda }, function (error, results, fields) {
+        this.mysql.query(query, { ...usuario }, function (error, results, fields) {
 
             if (error) {
                 return res.status(200).json({ code: 500, id: null, error: error })
@@ -40,28 +40,28 @@ class UsuarioController {
 
         var id = req.params.id
         if (!id) {
-            return res.status(500).json({ code: 500, venda: null, error: `id da ${this.tabela} não fornecido` })
+            return res.status(500).json({ code: 500, usuario: null, error: `id da ${this.tabela} não fornecido` })
         }
 
         this.mysql.query(`Select * from ${this.tabela} where id = ?`, id, function (error, results, fields) {
 
             if (error || !results.length) {
-                return res.status(200).json({ code: 500, venda: null, error: `${this.tabela} não encontrada` })
+                return res.status(200).json({ code: 500, usuario: null, error: `${this.tabela} não encontrada` })
             }
 
-            return res.status(200).json({ code: 200, venda: results[0], error: null })
+            return res.status(200).json({ code: 200, usuario: results[0], error: null })
         })
     }
 
     update(req, res, next) {
 
-        var venda = req.body.venda
-        if (!venda) {
+        var usuario = req.body.usuario
+        if (!usuario) {
             return res.status(500).json({ code: 500, resultado: null, error: `${this.tabela} não fornecido` })
         }
 
         var query = `update ${this.tabela} set nome = ?, cep = ?, cpf = ?, email = ? where id = ?`
-        this.mysql.query(query, [venda.nome, venda.id], function (error, results, fields) {
+        this.mysql.query(query, [usuario.nome, usuario.id], function (error, results, fields) {
 
             if (error) {
                 return res.status(200).json({ code: 500, resultado: null, error: `${this.tabela} não pode ser atualizado` })
@@ -76,10 +76,10 @@ class UsuarioController {
         this.mysql.query(`Select * from ${this.tabela}`, function (error, results, fields) {
 
             if (error) {
-                return res.status(200).json({ code: 500, vendas: null, error: `${this.tabela_plural} não encontrados` })
+                return res.status(200).json({ code: 500, usuarios: null, error: `${this.tabela_plural} não encontrados` })
             }
 
-            return res.status(200).json({ code: 200, vendas: results, error: null })
+            return res.status(200).json({ code: 200, usuarios: results, error: null })
         })
     }
 
