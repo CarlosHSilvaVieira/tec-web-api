efa86b7b5d3b97f8c1cd3b36032e179703b56ccb
Carlos <carlos.h.silva.vieira@gmail.com>
2019-05-31 11:16:29 -0300

    Change API port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var cors = require('cors');
 
 // criando app
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3005;
 
 app.use(bodyParse.json());
 app.use(cors());
